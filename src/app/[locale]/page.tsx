13c5f9a7c010de7baf94cd432a6b85533502d760
Gracecom <64633490+tomotech1@users.noreply.github.com>
2026-07-09 12:26:11 +0900

    test: minimal page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,25 +1,3 @@
-"use client"
-
-import HeroSection from "@/components/aesop/home/HeroSection"
-import MarqueeSection from "@/components/aesop/home/MarqueeSection"
-import ServicesSection from "@/components/aesop/home/ServicesSection"
-import ProcessSection from "@/components/aesop/home/ProcessSection"
-import StatsHeroicSection from "@/components/aesop/home/StatsHeroicSection"
-import FeaturesSection from "@/components/aesop/home/FeaturesSection"
-import TestimonialsSection from "@/components/aesop/home/TestimonialsSection"
-import CtaSection from "@/components/aesop/home/CtaSection"
-
 export default function HomePage() {
-  return (
-    <div className="overflow-x-hidden">
-      <HeroSection />
-      <MarqueeSection />
-      <ServicesSection />
-      <ProcessSection />
-      <StatsHeroicSection />
-      <FeaturesSection />
-      <TestimonialsSection />
-      <CtaSection />
-    </div>
-  )
+  return <h1 className="text-4xl p-20">Hello World</h1>
 }
